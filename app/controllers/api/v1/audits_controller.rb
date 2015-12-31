e4e52c5608e5620f7379cb99c461f23712f4f82e
Josh Woodward <jdw6359@rit.edu>
2015-12-31 12:59:02 -0500

    Audit creation implemented

--- app/controllers/api/v1/audits_controller.rb
+++ app/controllers/api/v1/audits_controller.rb
@@ -4,19 +4,18 @@ class Api::V1::AuditsController < ApplicationController
 	before_action :set_student, only: [:create]
 
 	def create
-		binding.pry
+		@student.audits.create(audit_params)
+		render json: {}, status: 200
 	end
 
 	private
 	def set_student
-
-		binding.pry
-
 		@student = Student.find(params[:student_id])
 	end
 
 	def audit_params
-		params.require(:student).permit(:student_id, :advisor_id, :date_performed, :comment)
+		params.require(:audit)
+			.permit(:date_performed, :comment)
+			.merge(:advisor_id => @current_user.id)
 	end
-
 end
