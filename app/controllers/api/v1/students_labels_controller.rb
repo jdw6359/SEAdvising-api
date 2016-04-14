51ed7707a0a5563ac709fe38dddeacd0b9a678ef
Joshua Woodward <jdw6359@rit.edu>
2016-04-14 19:00:12 -0400

    Remove binding from student_labels controller

--- app/controllers/api/v1/students_labels_controller.rb
+++ app/controllers/api/v1/students_labels_controller.rb
@@ -1,28 +1,26 @@
 class Api::V1::StudentsLabelsController < ApplicationController
 
-    before_action :set_student
+  before_action :set_student
 
-    def index
-        render json: @student.labels
+  def index
+    render json: @student.labels
+  end
+
+  def create
+    label = Label.find(params[:id])
+    @student.labels << label
+    if @student.save
+      render json: { status: 200 }
+    else
+      render json: { errors: @student.errors }
     end
+  end
 
-    def create
-        label = Label.find(params[:id])
-        @student.labels << label
-        if @student.save
-            render json: { status: 200 }
-        else
-            render json: { errors: @student.errors }
-        end
-    end
+  def destroy
+  end
 
-    def destroy
-        binding.pry
-
-    end
-
-    private
-    def set_student
-        @student = Student.find(params[:student_id])
-    end    
+  private
+  def set_student
+    @student = Student.find(params[:student_id])
+  end    
 end
